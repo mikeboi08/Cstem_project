3998cd07eb7ad4b6aa95cce8231a4d9919f821fe
mikeboi08 <micahhoward99@gmail.com>
2017-05-15 17:07:40 -0400

    Add files via upload

---
+++ puzzle_Questions.js
@@ -0,0 +1,88 @@
+var puzzle1 = {
+    "question": "A farmer has 17 sheep and all but 9 die. How many are left?",
+    "answer" : "9"};
+
+
+var puzzle2 = {
+  "question" : "In a year, there are 12 months. Seven months have 31 days. How many months have 28 days?",
+  "answer" : "12"
+}
+
+var puzzle3 = {
+  "question" : "What are the next three letters in the following sequence?J, F, M, A, M, J, J, A, ",
+  "answer" : "SON"
+
+}
+
+var puzzle4 = {
+  "question" : "Jimmy’s mother had four children. She named the first Monday. She named the second Tuesday, and she named the third Wednesday. What is the name of the fourth child?",
+  "answer" : "Jimmy"
+}
+
+var puzzle5 = {
+   "question" : "Which is heavier? A pound of feathers or a pound of rocks?",
+  "answer" : "both"
+}
+
+
+var puzzle6 = {
+   "question" : "What is full of holes but can still hold water?",
+  "answer" : "a sponge"
+}
+
+
+
+var puzzle7 = {
+   "question" : "A plane crashes on the border of the U.S. and Canada. Where do they bury the survivors?",
+  "answer" : "nowhere"
+}
+
+
+var puzzle8 = {
+   "question" : "I do not have any special powers, but I can predict the score of any football game before it begins. What is the score?",
+  "answer" : "0"
+}
+
+
+
+var puzzle9 = {
+   "question" : "I have kst in the middle. I begin with in. I end with and. What word am I?",
+  "answer" : "inkstand"
+}
+
+var puzzle10 = {
+   "question" : "I have kst in the middle. I begin with in. I end with and. What word am I?",
+  "answer" : "stop"
+}
+var puzzle11 = {
+   "question" : "Give me food, and I will live; give me water, and I will die. What am I?",
+  "answer" : "fire"
+}
+var puzzle12 = {
+   "question" : "Which travels faster? Hot or Cold?",
+  "answer" : "hot"
+}
+var puzzle13 = {
+   "question" : "When you have me, you immediately feel like sharing me. But, if you do share me, you do not have me.",
+  "answer" : "secret"
+}
+var puzzle14 = {
+   "question" : "What can you hold without ever touching or using your hands?",
+  "answer" : "breath"
+}
+var puzzle15 = {
+   "question" : "First, I threw away the outside and cooked the inside. Then I ate the outside and threw away the inside. What did I eat?",
+  "answer" : "corn on the cob"
+}
+var puzzle16 = {
+   "question" : "There are three houses. One is red, one is blue, and one is white. If the red house is to the left of the house in the middle, and the blue house is to the right to the house in the middle, where is the white house?",
+  "answer" : "Washington DC"
+}
+var puzzle17= {
+   "question" : "You are in a cabin and it is pitch black. You have one match on you. Which do you light first, the newspaper, the lamp, the candle, or the fire?",
+  "answer" : "match"
+}
+var puzzle18 = {
+   "question" : " Who is bigger: Mr. Bigger, Mrs. Bigger, or their baby?",
+  "answer" : "baby"
+}
